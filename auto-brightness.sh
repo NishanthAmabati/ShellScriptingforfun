09413aa47170937058475322684081c9e6fb8db7
Nishanth Ambati <133779317+NishanthAmabati@users.noreply.github.com>
2024-04-12 09:19:32 +0530

    Add files via upload

---
+++ auto-brightness.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+# max: 5000, min: 3000
+
+sudo su
+brightnessfile="/sys/class/backlight/intel_backlight/brightness"
+currentbrightness="$(cat $brightnessfile)"
+
+pm () {
+        if [[ $currentbrightness -gt 3000 ]]; then
+                for (( i=$currentbrightness; i>=3000; i-=300 )); do
+                        echo $i > /sys/class/backlight/intel_backlight/brightness
+                        sleep 0.5 
+                done
+        fi
+}
+
+am () {
+        if [[ $currentbrightness -lt 3000 ]]; then
+                for (( i=$currentbrightness; i<=5000; i+=300 )); do
+                        echo $i > /sys/class/backlight/intel_backlight/brightness
+                        sleep 0.5 
+                done
+        fi
+}
+
+# check AM or PM and call 
+if [[ $(date +%p) == "AM" ]]; then am; else pm; fi
